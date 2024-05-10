8f553056b7690c704501c246e1f4f924d059d1c3
Willoz12 <william.pecci@studio.unibo.it>
2024-05-10 09:09:09 +0200

    0 Autograd Inizio.py

--- Video Karpathy/0 Autograd Inizio.py
+++ Video Karpathy/0 Autograd Inizio.py
@@ -4,10 +4,9 @@ install micrograd
 from micrograd.engine import Value
 
 #tecnicamente dovrei fare a = Value(-4.0) e b = Value(2.0), ovvero trasformare questi in un valore oggetto chiamato "Value".
-#Non potendo importare il pacchetto non so come impostarlo. I numeri dovrebbero comunque ricondurre a sé stessi
+#Non potendo importare il pacchetto non so come impostarlo, ma è solo un esempio per far capire come funziona
 #Costruiamo un'equazione dove 'a' e 'b' sono trasformati in c,d,e...
 #Micrograd supporta diversi tipi di operazioni:
-
 #Sommare e moltiplicare valori, elevarli a potenza, operatore di incremento (+=), porre in negativo (-a), dividere per una costante (f/2)
 
 #L'idea è di creare un'espressione che, dati i valori 'a' e 'b', restituisca un output 'g' che in background Micrograd costruirà questa espressione
@@ -26,9 +25,7 @@ e = c - d
 f = e**2
 g = f/2.0
 g += 10.0/f
-print(f'{g.data:.4f}') #Viene vuori essere 24.7041 #mi dice però che non può farlo perché non ho importato il pacchetto
-#g.backward() #Parte da 'g' e poi inizia ad andare indietro e applica la catena di calcolo. Quindi valuta la derivata di g rispetto ai nodi interni, come 'c' e 'd',
-#ma anche rispetto agli input come 'a' e 'b'. Ovviamente non mi funziona, in questo caso perché "'float' object has no attribute 'backward'"
+print(f'{g.data:.4f}') #Viene vuori essere 24.7041 #mi dice però che non può farlo perché non ho importato il pacchetto"
 print(f'{a.grad:.4f}') #viene 138.8338, ovvero la derivata di dg/da
 print(f'{b.grad:.4f}') #viene 645.5773, ovvero la derivata di dg/db
 
